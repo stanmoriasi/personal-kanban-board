87fc0c22d4019ddd3753a94369b3fbef6ba76ef2
stanmoriasi <stanmoriasi@yahoo.com>
2025-02-14 23:21:53 -0700

    add check for DB_URL

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -5,15 +5,18 @@ import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+let sequelize: Sequelize;
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD || '', {
+    host: 'localhost',
+    dialect: 'postgres',
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  });
 
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
