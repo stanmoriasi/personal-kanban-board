4b01d9a315315b54bd742e5484f9426b03a148f4
stanmoriasi <stanmoriasi@yahoo.com>
2025-02-14 22:57:40 -0700

    add route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -7,6 +7,7 @@ const router = Router();
 
 router.use('/auth', authRoutes);
 // TODO: Add authentication to the API routes
+router.use(authenticateToken);
 router.use('/api', apiRoutes);
 
 export default router;
