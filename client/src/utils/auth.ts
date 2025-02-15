ccf564b40fa5d96b363d09b715c4a53890522a2f
stanmoriasi <stanmoriasi@yahoo.com>
2025-02-14 22:42:38 -0700

    update authservice routes to obtain and check JWT token

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,28 +3,40 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    return jwtDecode<JwtPayload>(this.getToken());
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    return !!this.getToken() && !this.isTokenExpired(this.getToken());
   }
   
-  isTokenExpired(token: string) {
+  isTokenExpired(token: string): boolean {
     // TODO: return a value that indicates if the token is expired
+    const decoded = jwtDecode<JwtPayload>(token);
+    if (decoded.exp) {
+      return decoded.exp < Date.now() / 1000;
+    }
+    return false;
   }
 
   getToken(): string {
     // TODO: return the token
+    return localStorage.getItem('id_token') || '';
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
+    localStorage.setItem('id_token', idToken);
     // TODO: redirect to the home page
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
+    localStorage.removeItem('id_token');
     // TODO: redirect to the login page
+    window.location.assign('/login');
   }
 }
 
